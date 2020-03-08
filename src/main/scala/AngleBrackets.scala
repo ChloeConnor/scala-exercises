cabf1da8cfc9dc29ab23391e7dadf0ea69c18909
chloeconnor <chloe.connor@vortexa.com>
2020-03-08 12:51:09 +0000

    angle brackets

---
+++ src/main/scala/AngleBrackets.scala
@@ -0,0 +1,35 @@
+object AngleBrackets {
+
+  def closeAllAngleBrackets(angles: String): String = {
+    val brackets = angles.toList.map(a => a.toString)
+
+    val requiredBrackets = brackets.zipWithIndex
+      .map(i => {
+        if (brackets
+          .lift(i._2)
+          .contains("<") && brackets.lift(i._2 + 1).contains(">")) "p"
+        else if (brackets.lift(i._2 - 1).contains("<") && brackets
+          .lift(i._2)
+          .contains(">")) "p"
+        else if (brackets.lift(i._2).contains(">")) "<"
+        else if (brackets.lift(i._2).contains("<")) ">"
+        else "w"
+      })
+      .filter(_ != "p")
+      .groupBy(_ == "<")
+
+    val start =
+      requiredBrackets.filter(a => a._1).values.toList.flatten.mkString("")
+
+    val end =
+      requiredBrackets.filter(a => !a._1).values.toList.flatten.mkString("")
+
+    start + brackets.mkString("") + end
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    println(closeAllAngleBrackets("><<><"))
+    assert(closeAllAngleBrackets("><<><") == "<><<><>>")
+  }
+}
