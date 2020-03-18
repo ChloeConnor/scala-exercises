6815f8ffd7dc45b954a8d163d5694ab9a55c9bc1
chloeconnor <chloe.connor@vortexa.com>
2020-03-18 09:46:04 +0000

    list from list

---
+++ src/main/scala/com/chloe/scala/ListFromList.scala
@@ -0,0 +1,33 @@
+package com.chloe.scala
+
+object ListFromList {
+
+  def returnNumberOfElements(list: List[String], number: Int): List[String] = {
+    list.slice(0, number)
+  }
+
+  def returnNumberOfElementsWhenLarger(list: List[String],
+                                       number: Int): List[String] = {
+    val lengthOfList = list.size
+    val howManyLists = (number / lengthOfList).floor.intValue()
+
+    val leftover = number - (howManyLists * lengthOfList)
+
+    val newList = List
+      .range(0, howManyLists)
+      .flatMap(l => {
+        list
+      })
+
+    newList ::: newList.slice(0, leftover)
+  }
+
+  def createList(list: List[String], number: Int): List[String] = {
+
+    if (list.size <= number) returnNumberOfElements(list, number)
+    else returnNumberOfElementsWhenLarger(list, number)
+
+  }
+
+  def main(args: Array[String]): Unit = {}
+}
---
+++ src/test/scala/com/chloe/scala/TestListFromList.scala
@@ -0,0 +1,28 @@
+package com.chloe.scala
+import com.chloe.scala.ListFromList._
+import org.scalatest.flatspec.AnyFlatSpec
+
+
+class TestListFromList extends AnyFlatSpec {
+
+  "List" should "return the number of elements given" in {
+    val inputList = List("apple", "pear", "lemon", "orange")
+    val inputNumber =  2
+    val expectedOutput = List("apple", "pear")
+
+    val output =returnNumberOfElements(inputList, inputNumber)
+
+    assert(output == expectedOutput)
+  }
+
+  it should "return the number of elements given if list is longer" in {
+    val inputList = List("apple", "pear", "lemon", "orange")
+    val inputNumber =  6
+    val expectedOutput = List("apple", "pear", "lemon", "orange", "apple", "pear")
+
+    val output = returnNumberOfElementsWhenLarger(inputList, inputNumber)
+
+    assert(output == expectedOutput)
+  }
+
+}
